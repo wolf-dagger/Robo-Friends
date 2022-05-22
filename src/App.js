1da93e1c3b95155f1cbc2a2d39ab9b0e9547f382
wolf-dagger <cu.17bcs4556@gmail.com>
2022-05-22 12:45:39 +0530

    Added events

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-    <div>
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
-    </div>
-    );
+const state = {
+    
+}
+class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            robots : robots,
+            searchfield: ''
+        }
+    }
+    onSearchChange (event) {
+        console.log(event.target.value);
+    }
+    render(){
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots}/>
+            </div>
+            );
+    }
+    
 }
 
 export default App;
