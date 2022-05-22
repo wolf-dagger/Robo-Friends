3ec39bf17300c22a67decda42d9991ad40e9f66c
wolf-dagger <cu.17bcs4556@gmail.com>
2022-05-22 12:10:14 +0530

    Added search box and title

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots';
-import CardList from './CardList';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots}/>
+    <div>
+      <App />
+    </div>
   </React.StrictMode>
 );
 
